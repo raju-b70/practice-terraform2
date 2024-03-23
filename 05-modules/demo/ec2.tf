267d145d8d5b4d4f9a53d1d7a069cd7b0b87a5fe
rajendra <rajendrammarboyana@gmail.com>
2024-03-23 16:58:05 +0000

    modules/functions demo

--- 05-modules/demo/ec2.tf
+++ 05-modules/demo/ec2.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "test" {
-  ami           = "ami-05f020f5935e52dc4"
-  instance_type = "t3.micro"
+  ami           = var.ami
+  instance_type = var.instance_type
   tags = {
     name = var.name
 
   }
 }
 variable "name" {}
+variable "instance_type" {}
+variable "ami" {}
--- 05-modules/main.tf
+++ 05-modules/main.tf
@@ -1,8 +1,13 @@
 module "test1" {
   source = "./demo"
   name = "demo-1"
+  instance_type = "t3.micro"
+  ami = "ami-05f020f5935e52dc4"
 }
 module "test2" {
   source = "./demo"
   name = "demo-2"
+  instance_type = "t3.micro"
+  ami = "ami-05f020f5935e52dc4"
+
 }
