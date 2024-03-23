cdd53788ac811f5d107922301785484ff8c236bb
rajendra <rajendrammarboyana@gmail.com>
2024-03-23 17:27:02 +0000

    modules/functions demo

--- 07-loops/main.tf
+++ 07-loops/main.tf
@@ -2,3 +2,16 @@
 resource "null_resource" "test" {
   count = 15
 }
+
+resource "null_resource" "test1" {
+  for_each = var.fruit-variety
+}
+
+variable "fruit-variety" {
+  default = {
+    orange = 2
+    banana = 3
+    pulp = 8
+    apple = 7
+  }
+}
