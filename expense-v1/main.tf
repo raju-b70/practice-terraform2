8e02f97497e51bc6b5efd982d7a23ce80c1df62d
rajendra <rajendrammarboyana@gmail.com>
2024-03-23 06:29:57 +0000

    creating instances for expense project

---
+++ expense-v1/main.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09998e9d2f260cf52"]
+  tags = {
+    name= "frontend"
+  }
+}
+
+resource "aws_instance" "backend" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09998e9d2f260cf52"]
+  tags = {
+    name= "backend"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-05f020f5935e52dc4"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-09998e9d2f260cf52"]
+  tags = {
+    name= "mysql"
+  }
+}
